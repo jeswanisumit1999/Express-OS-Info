b7a397d34dc9716861a79704ce97e4aa9b08cad8
Sumit Jeswani <jeswanisumit1999@gmail.com>
2023-08-11 00:49:35 +0530

    Update to HTML View

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const os = require('os');
 
 const app = express();
-const port = 80;
+const port = 3000;
 
 app.get('/', (req, res) => {
   const serverInfo = {
@@ -10,10 +10,26 @@ app.get('/', (req, res) => {
     architecture: os.arch(),
     release: os.release(),
     totalMemory: os.totalmem(),
-    freeMemory: os.freemem(),
+    freeMemory: os.freemem()
   };
-
-  res.json(serverInfo);
+  const htmlResponse = `
+  <html>
+    <head>
+      <title>System and OS Information</title>
+    </head>
+    <body>
+      <h1>System Information</h1>
+      <h2>Platform : ${serverInfo.platform}</h2>
+      <h2>Architecture : ${serverInfo.architecture}</h2>
+      <h2>Release : ${serverInfo.release}</h2>
+      <h2>Total Memory : ${serverInfo.totalMemory}</h2>
+      <h2>Free Memory : ${serverInfo.freeMemory}</h2>
+    </body>
+  </html>
+`;
+  res.send(
+    htmlResponse
+  );
 });
 
 app.listen(port, () => {
